4b39b8cb1db420b91930d082499bc4839692e2df
rickyz22 <rickyz1749@gmail.com>
2026-03-10 23:16:46 -0300

    Update fetch event listener for service worker

--- sw.js
+++ sw.js
@@ -1,7 +1,3 @@
-self.addEventListener('install', (e) => {
-  console.log('SW instalado');
-});
-
-self.addEventListener('fetch', (e) => {
-  // Necesario para que Chrome habilite el modo App
+self.addEventListener('fetch', (event) => {
+  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
 });
